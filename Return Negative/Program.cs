78d5defb1e6ca31091f4e0ae82f8088ac106f3b2
Kraichikk <108270237+Kraichikk@users.noreply.github.com>
2023-02-24 10:41:54 +0200

    Add project files.

---
+++ Return Negative/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Return_Negative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+        public static int MakeNegative(int number)
+        {
+            if (number <= 0)
+            {
+                return number;
+            }
+            else
+            {
+                return number * -1;
+            }
+        }
+    }
+}
